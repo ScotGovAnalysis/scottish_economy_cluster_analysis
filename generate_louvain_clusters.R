4d71e0f5c7f52b7d6bd105a847221bed3423fb1e
GordonBryden <gordon.bryden@gmail.com>
2019-09-25 11:32:08 +0100

    Update and rename louvain_application.R to generate_louvain_clusters.R

---
+++ generate_louvain_clusters.R
@@ -0,0 +1,29 @@
+library(dplyr)
+library(NetworkToolbox)
+
+#load data
+leontief <- read.csv("./data/leon4_no_bad_data.csv")
+
+#remove diagonals and name column
+leontief <- leontief %>% select(-Row.Labels)
+leontief[leontief >= 1000] <- 0
+
+# remove uninformative sectors
+leon2 <- leontief %>% 
+  slice(-match(c("Tobacco",
+                 "Imputed.rent",
+                 "Households.as.employers"),
+               colnames(leontief))) %>%
+  select(-Tobacco,
+         -Imputed.rent,
+         -Households.as.employers)
+
+#run community function
+community <- louvain(leon2, 1.8)$community
+
+#create user readable tables
+community_names <- community %>%
+  cbind(colnames(leon2)) %>%
+  as.data.frame()
+
+colnames(community_names) <- c("cluster_id","sector")
